9895fc750a1e00dd522452d7015a2e43d41d9297
Danielle-Luz <daniln2209@gmail.com>
2023-02-06 01:52:04 -0300

    feat: add domain env variable

--- src/emailConfig.ts
+++ src/emailConfig.ts
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 
 export const emailSender = nodemailer.createTransport({
+  domain: process.env.DOMAIN,
   host: process.env.HOST,
   port: 587,
   auth: {
